1e276f7fbb682d968effbcfd54837acf77f81fab
Barry-owino <barryowino5@gmail.com>
2025-07-04 21:07:27 +0300

    added the EXPLAIN ANALYZE

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -11,3 +11,24 @@ CREATE INDEX idx_properties_title ON Properties (title);
 -- Useful for queries that order reviews by date, especially when combined with other filters.
 CREATE INDEX idx_reviews_reviewDate ON Reviews (reviewDate DESC);
 
+EXPLAIN ANALYZE
+SELECT
+    p.listingId,
+    p.title AS property_title,
+    p.city,
+    p.pricePerNight,
+    b.bookingId,
+    b.checkInDate,
+    b.checkOutDate,
+    r.reviewId,
+    r.rating,
+    r.comment,
+    r.reviewDate
+FROM
+    Properties AS p
+LEFT JOIN
+    Bookings AS b ON p.listingId = b.propertyId
+LEFT JOIN
+    Reviews AS r ON b.bookingId = r.bookingId
+ORDER BY
+    p.title, r.reviewDate DESC;
